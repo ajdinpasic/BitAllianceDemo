6f16102320cda5b7d2fc0fb4da2554312f269725
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2023-04-28 15:00:02 +0200

    Maps implemented in profile

---
+++ LibraryManagement.AutoMapperProfile/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManagement.BLL.Data;
+using LibraryManagement.DAL.Data;
+
+namespace LibraryManagement.AutoMapperProfile
+{
+    public class AutoMapperProfile : Profile 
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Book, BookDTO>();
+        }
+    }
+}
